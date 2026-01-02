5bc34df625ac2eca9baf3ddfa152080ecea81281
gauravne <gauravneware1997@gmail.com>
2026-01-03 00:59:31 +0530

    Hello Gaurav

---
+++ src/test/java/baseTest/HelloWord.java
@@ -0,0 +1,12 @@
+package baseTest;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello Gaurav !!!");
+	}
+
+}
